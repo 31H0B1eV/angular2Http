6d433dca0c8879b10e8ddfac81244e201dec0f4d
Artem Zinoviev <arzinoviev@gmail.com>
2016-07-28 15:38:04 +0300

    remove dummy readme and simple test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,14 @@
 
 describe('QuickStart E2E Tests', function () {
 
-  let expectedMsg = 'My First Angular 2 App';
-
+  let expectedMsg = 'Followers';
 
   beforeEach(function () {
     browser.get('');
   });
 
   it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+    expect(element(by.css('h3')).getText()).toEqual(expectedMsg);
   });
 
 });
